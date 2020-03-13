5f1da56912b8c63eb2d706deb8c099caa69681bb
Sophia Saiada <sophia.saiada@gmail.com>
2020-03-13 18:51:00 +0200

    Make page aligned RTL

---
+++ admin/src/ThemeApp.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import App from "./App";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { heIL } from "@material-ui/core/locale";
+import { create } from "jss";
+import rtl from "jss-rtl";
+import { StylesProvider, jssPreset } from "@material-ui/core/styles";
+
+const theme = createMuiTheme(
+  {
+    direction: "rtl",
+    palette: {
+      primary: { main: "#8e24aa" }
+    }
+  },
+  heIL
+);
+
+// Configure JSS
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+
+type RTLProps = {
+  children: any;
+};
+
+const RTL = (props: RTLProps) => {
+  return <StylesProvider jss={jss}>{props.children}</StylesProvider>;
+};
+
+const ThemedApp = () => (
+  <RTL>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </RTL>
+);
+
+export default ThemedApp;
