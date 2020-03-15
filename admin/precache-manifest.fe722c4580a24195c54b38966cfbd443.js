5816a5d28659c8373c057b8a1c1c5de6d033e06e
Sophia Saiada <sophia.saiada@gmail.com>
2020-03-16 00:48:54 +0200

    Update admin build

---
+++ admin/precache-manifest.fe722c4580a24195c54b38966cfbd443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9809892a59237af5b9286d22baf18dea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1028788f9f8e646495dd",
+    "url": "/static/css/main.3cedba41.chunk.css"
+  },
+  {
+    "revision": "f54739772d97244b26b0",
+    "url": "/static/js/2.472b020c.chunk.js"
+  },
+  {
+    "revision": "b68ea9bfcdfe88fd25eac9df29279526",
+    "url": "/static/js/2.472b020c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1028788f9f8e646495dd",
+    "url": "/static/js/main.de4a27c7.chunk.js"
+  },
+  {
+    "revision": "903c8d53aa51e13a5437",
+    "url": "/static/js/runtime-main.4c78ebac.js"
+  },
+  {
+    "revision": "565a7ac6a03a3fc17db633a33970f46e",
+    "url": "/static/media/SuccessIllustration.565a7ac6.svg"
+  }
+]);
