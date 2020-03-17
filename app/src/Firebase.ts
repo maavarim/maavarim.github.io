af257b0f237c4dfd9b9a549ae49d943e3d997708
Sophia Saiada <sophia.saiada@gmail.com>
2020-03-17 21:55:18 +0200

    Add Firebase

---
+++ app/src/Firebase.ts
@@ -0,0 +1,18 @@
+import * as firebase from "firebase/app";
+
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0e79ilwZg5BVUUOrYB6iAj1w0FZiAkcU",
+  authDomain: "maavarimfriendly.firebaseapp.com",
+  databaseURL: "https://maavarimfriendly.firebaseio.com",
+  projectId: "maavarimfriendly",
+  storageBucket: "maavarimfriendly.appspot.com",
+  messagingSenderId: "479517849359",
+  appId: "1:479517849359:web:998d592de50fe007b4707a",
+  measurementId: "G-5YNF4DPSTE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
