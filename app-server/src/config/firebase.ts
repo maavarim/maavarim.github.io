b24176d648bf0ddcf4046e4ebcb375e066262217
Sophia Saiada <sophia.saiada@gmail.com>
2020-03-23 18:40:01 +0200

    Attempt to fix firebase initialization

--- app-server/src/config/firebase.ts
+++ app-server/src/config/firebase.ts
@@ -2,5 +2,10 @@ require("dotenv").config();
 import * as admin from "firebase-admin";
 
 export default admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.cert({
+    projectId: "maavarimfriendly",
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+  }),
+  databaseURL: "https://maavarimfriendly.firebaseio.com"
 });
