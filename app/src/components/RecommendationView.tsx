d5f77e17b3be83ac0d5a673a01f764c08a57a03d
Sophia Saiada <sophia.saiada@gmail.com>
2020-03-28 16:41:37 +0300

    Display search results, focus on a specific result when get clicked

--- app/src/components/RecommendationView.tsx
+++ app/src/components/RecommendationView.tsx
@@ -1,18 +1,12 @@
-import React from "react";
-import {
-  Typography,
-  makeStyles,
-  Box,
-  Card,
-  CardContent
-} from "@material-ui/core";
+import React, { Fragment } from "react";
+import { Typography, makeStyles, Box, Button } from "@material-ui/core";
 import {
   PlaceTwoTone,
   LabelTwoTone,
   CallTwoTone,
   TranslateTwoTone,
-  HealingTwoTone,
-  SpaTwoTone
+  SpaTwoTone,
+  BookTwoTone
 } from "@material-ui/icons";
 import Rating from "./Rating";
 import ServerRecommendation from "../types/ServerRecommendation";
@@ -24,13 +18,11 @@ const useStyles = makeStyles(theme => ({
   },
   name: {
     flexGrow: 1,
-    marginRight: theme.spacing(1),
-    lineHeight: "1em",
-    marginBottom: ".25em"
+    marginRight: theme.spacing(2),
+    lineHeight: "1em"
   },
   rating: {
-    fontSize: "1.1em",
-    marginBottom: ".25em"
+    fontSize: "1.1em"
   },
   tinyText: {
     fontSize: ".8em"
@@ -39,56 +31,104 @@ const useStyles = makeStyles(theme => ({
     display: "grid",
     gridTemplateColumns: "1.1em 1fr",
     gridGap: ".25em .5em",
-    marginTop: ".25em",
     "& .MuiSvgIcon-root": {
       fontSize: "1.1em",
       height: "1.1em",
       width: "1.1em"
+    }
+  },
+  buttonsConatiner: {
+    display: "grid",
+    gridTemplateColumns: "1fr",
+    [theme.breakpoints.up("xs")]: {
+      gridTemplateColumns: "repeat(2, 1fr)",
+      borderTop: "1px solid #f7ecf9",
+      "& .MuiButton-root": {
+        borderRadius: 0
+      }
+    }
+  },
+  moreDetailsButton: {
+    background: "#f9f9f9",
+    color: "#8e24aa"
+  },
+  addButton: {
+    background: "#9c27b0",
+    color: "#f1efef",
+    "&:hover": {
+      background: "rgba(156, 39, 176, 0.85)"
     }
   }
 }));
 
 interface RecommendationViewProps {
   recommendation: ServerRecommendation;
+  onShowMoreDetails?: () => void;
+  detailed?: boolean;
 }
 
-const RecommendationView = ({ recommendation }: RecommendationViewProps) => {
+const RecommendationView = ({
+  recommendation,
+  onShowMoreDetails,
+  detailed = false
+}: RecommendationViewProps) => {
   const classes = useStyles();
 
   return (
-    <Card>
-      <CardContent style={{ padding: 0 }}>
-        <Box className={classes.container} p={2}>
-          <Box display="flex" alignItems="center" flexWrap="wrap">
-            <Typography variant="h6" className={classes.name}>
-              {recommendation.name}
-            </Typography>
-            <Rating
-              className={classes.rating}
-              value={recommendation.rating}
-              readOnly
-            />
-          </Box>
-
-          <Box className={classes.details}>
-            <LabelTwoTone />
-            <Box>{formatForDisplaying(recommendation.profession)}</Box>
-
-            <PlaceTwoTone />
-            <Box>{recommendation.location}</Box>
-
-            <CallTwoTone />
-            <Box>{recommendation.phone}</Box>
-
-            <TranslateTwoTone />
-            <Box>{formatForDisplaying(recommendation.language)}</Box>
-
-            <SpaTwoTone />
-            <Box>{formatForDisplaying(recommendation.healthCare)}</Box>
-          </Box>
+    <Fragment>
+      <Box className={classes.container} p={2} pb={1}>
+        <Box display="flex" alignItems="center" flexWrap="wrap" mb={1}>
+          <Typography variant="h6" className={classes.name}>
+            {recommendation.name}
+          </Typography>
+          <Rating
+            className={classes.rating}
+            value={recommendation.rating}
+            readOnly
+          />
         </Box>
-      </CardContent>
-    </Card>
+
+        <Box className={classes.details}>
+          <LabelTwoTone />
+          <Box>{formatForDisplaying(recommendation.profession)}</Box>
+
+          {detailed && (
+            <Fragment>
+              <BookTwoTone />
+              <Box>{formatForDisplaying(recommendation.expertise)}</Box>
+            </Fragment>
+          )}
+
+          <PlaceTwoTone />
+          <Box>{recommendation.location}</Box>
+
+          <CallTwoTone />
+          <Box>{recommendation.phone}</Box>
+
+          <TranslateTwoTone />
+          <Box>{formatForDisplaying(recommendation.language)}</Box>
+
+          <SpaTwoTone />
+          <Box>{formatForDisplaying(recommendation.healthCare)}</Box>
+        </Box>
+      </Box>
+      {!detailed && (
+        <Box className={classes.buttonsConatiner}>
+          {onShowMoreDetails && (
+            <Button
+              size="small"
+              className={classes.moreDetailsButton}
+              onClick={onShowMoreDetails}
+            >
+              פרטים נוספים
+            </Button>
+          )}
+          <Button size="small" className={classes.addButton}>
+            יש לי מה להוסיף!
+          </Button>
+        </Box>
+      )}
+    </Fragment>
   );
 };
 
--- app/src/components/SearchContainer.tsx
+++ app/src/components/SearchContainer.tsx
@@ -8,7 +8,8 @@ import {
   CardContent,
   TextField,
   InputAdornment,
-  Button
+  Button,
+  Dialog
 } from "@material-ui/core";
 import RecommendationView from "./RecommendationView";
 import HomeSearchIcon from "./custom-icons/HomeSearchIcon";
@@ -78,7 +79,85 @@ function SearchContainer() {
     selectedFilterApplicatorsReducer,
     new Map<string, string[]>()
   );
-  const [results, setResults] = useState<ServerRecommendation[] | null>(null);
+  const [results, setResults] = useState<ServerRecommendation[] | null>([
+    {
+      area: ["גוש דן", "אזור אשקלון"],
+      expertise: ["אנדוקרינולוגיה", "ילדים ומתבגרים", "משפחה"],
+      gender: ["גבר"],
+      healthCare: ["מכבי"],
+      language: ["עברית"],
+      profession: ["עוסק.ת מתחום הרפואה", "אחר"],
+      accepted: true,
+      _id: "5e7a3268e9ad56f642d738aa",
+      authorEmail: "sophia@maavarim.com",
+      name: "דר׳ פרינדלי",
+      rating: 4,
+      phone: "0538306666",
+      location: "ג׳יי קיי רולינג 5, תל אביב-יפו",
+      additionalInfo: "הוא ממש נחמד",
+      createdAt: new Date("2020-03-26T15:59:50.467Z")
+    },
+    {
+      area: ["גוש דן", "אזור נתניה"],
+      expertise: ["אנדוקרינולוגיה", "משפחה"],
+      gender: ["אישה"],
+      healthCare: ["מכבי", "לאומית"],
+      language: ["ערבית"],
+      profession: ["עוסק.ת מתחום הרפואה"],
+      accepted: true,
+      _id: "5e7a32e51c6542f6680635ae",
+      authorEmail: "sophia@maavarim.com",
+      name: "דר׳ פרינדלית עם ",
+      rating: 5,
+      phone: "0538307777",
+      location: "ג׳יי קיי רולינג 2, נתניה",
+      additionalInfo: "היא סופר נחמדה",
+      createdAt: new Date("2020-03-26T12:29:50.467Z")
+    },
+    {
+      area: ["אזור נתניה", "גוש דן"],
+      expertise: ["אחר", "ילדים ומתבגרים", "טיפול באמנות"],
+      gender: ["אישה"],
+      healthCare: ["מכבי"],
+      language: ["עברית", "אנגלית"],
+      profession: ["אחר"],
+      accepted: true,
+      _id: "5e7e067e66cc115be8f04765",
+      authorEmail: "sophia@maavarim.org",
+      name: "סופיה ציאדה",
+      rating: 4,
+      phone: "07989799",
+      location: "הבית של סופי",
+      additionalInfo: "עוד פרטים",
+      createdAt: new Date("2020-03-27T12:59:50.467Z")
+    },
+    {
+      area: ["אזור נתניה"],
+      expertise: ["הסרת שיער בלייזר"],
+      gender: ["אחר"],
+      healthCare: ["מכבי"],
+      language: ["אנגלית"],
+      profession: ["מטפל.ת"],
+      accepted: true,
+      _id: "5e7e22f6ad70306284424f6b",
+      authorEmail: "sophia@maavarim.org",
+      name: "test",
+      rating: 4,
+      phone: "sd",
+      location: "sds",
+      additionalInfo: "sd",
+      createdAt: new Date("2020-03-27T15:59:50.467Z")
+    }
+  ]);
+
+  const [
+    focusedRecommendation,
+    setFocusedRecommendation
+  ] = useState<ServerRecommendation | null>(null);
+  const [
+    isFocusedRecommendationDialogOpen,
+    setIsFocusedRecommendationDialogOpen
+  ] = useState(false);
 
   const fetchResults = () => {
     const searchQuery = {
@@ -150,14 +229,36 @@ function SearchContainer() {
         {results !== null && (
           <Box className={classes.resultsContainer} mt={2} mb={2}>
             {results.map(recommendation => (
-              <RecommendationView
-                key={recommendation._id}
-                recommendation={recommendation}
-              />
+              <Card key={recommendation._id}>
+                <CardContent style={{ padding: 0 }}>
+                  <RecommendationView
+                    recommendation={recommendation}
+                    onShowMoreDetails={() => {
+                      setFocusedRecommendation(recommendation);
+                      setIsFocusedRecommendationDialogOpen(true);
+                    }}
+                  />
+                </CardContent>
+              </Card>
             ))}
           </Box>
         )}
       </Container>
+      <Dialog
+        open={isFocusedRecommendationDialogOpen}
+        onClose={() => setIsFocusedRecommendationDialogOpen(false)}
+        maxWidth="sm"
+        fullWidth
+      >
+        <Box p={1} pb={2}>
+          {focusedRecommendation && (
+            <RecommendationView
+              recommendation={focusedRecommendation}
+              detailed
+            />
+          )}
+        </Box>
+      </Dialog>
     </Fragment>
   );
 }
